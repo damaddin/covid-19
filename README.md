3417087d38d32cbebe3359c023a06dada778fb1b
mst <mst@mstsoft.com>
2020-04-03 23:54:55 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # covid-19
-My Take on a Visualization of COVID-19 Data
+
+Another take on a visualization of COVID-19 Data. Trying to extrapolate with a simple model on current data. Feel free to suggest changes.
+
+# Acknowledgements
+
+* Code forked / hacked from https://felipec.github.io/covid-19/ for visuals
+* Data provided by https://github.com/pomber/covid19 which pulls from Johns Hopkins.
---
+++ util.js
@@ -0,0 +1,97 @@
+var number = new Intl.NumberFormat();
+
+var chart = new Chart('chart', {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [],
+  },
+  options: {
+    resposive: true,
+    maintainAspectRatio: false,
+    aspectRatio: 0.5,
+    elements: {
+      line: {
+        fill: false,
+        borderWidth: 2,
+        lineTension: 0,
+      },
+    },
+    tooltips: {
+      mode: 'x',
+      bodyAlign: 'right',
+      itemSort: (a, b) => b.yLabel - a.yLabel,
+      callbacks: {
+        label: (item, data) => {
+          return data.datasets[item.datasetIndex].label + ': ' + number.format(item.yLabel);
+        },
+      },
+    },
+    scales: {
+      xAxes: [{
+        scaleLabel: { labelString: 'days', display: true, },
+      }],
+      yAxes: [{
+        scaleLabel: { display: true, },
+        ticks: {
+          callback: (value) => number.format(value),
+        },
+      }],
+    },
+  },
+});
+
+async function start(callback, callback2) {
+  const response = await fetch('https://pomber.github.io/covid19/timeseries.json');
+  const data = await response.json();
+
+  let keys = Array.from( Object.keys(data) );
+
+  let countries = [
+  ];
+
+  for (var i = 0; i < keys.length; i++) {
+    countries.push({ id : keys[i], color: 'hsl(280, 75%, 50%)' });
+  }
+
+  if (r = window.location.href.match(/[?&]country=([^&]+)/)) {
+    const n = {
+      id: r[1],
+      color: 'hsl(280, 75%, 50%)',
+      params: { borderWidth: 4, pointStyle: 'rectRot' },
+    };
+    countries = countries.filter(e => e.id == n.id);
+  }
+
+  for (const {id, color, params} of countries) {
+    const dataset = {
+      label: id,
+      data: callback(data[id]),
+      borderColor: color,
+      backgroundColor: color,
+    };
+
+    if (params) Object.assign(dataset, params);
+    chart.data.datasets.push(dataset);
+
+    const dataset2 = {
+      label: "predition",
+      data: callback2(data[id]),
+      borderColor: 'hsl(180, 75%, 50%)',
+      backgroundColor: 'hsl(180, 75%, 50%)',
+    };
+
+    if (params) Object.assign(dataset2, params);
+    chart.data.datasets.push(dataset2);
+  }
+
+  const max_days = chart.data.datasets.reduce((accum, e) => Math.max(accum, e.data.length), 0);
+  chart.data.labels = Array.apply(null, Array(max_days)).map((e, i) => i + 1);
+  const axis = chart.options.scales.yAxes[0];
+  if (axis.type == 'logarithmic') {
+    axis.ticks.callback = (value) => {
+      return Number.isInteger(Math.log10(value)) ? number.format(value) : null;
+    };
+  }
+  chart.update();
+}
